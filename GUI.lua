9084a9d05553bf53d614041f581a228edba814fb
PetKunggamer <57756821+PetKunggamer@users.noreply.github.com>
2022-04-20 00:56:14 +0700

    Add files via upload

---
+++ GUI.lua
@@ -0,0 +1,44 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/6W1ZqV53"))()
+
+local w = Library:Window("Chaiwat Hub")
+
+local p = Library:Window("Auto Buy")
+
+local c = Library:Window("Item Farm")
+
+c:Toggle("Farm", function(v)
+    getgenv().ItemFarm = v
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/PetKunggamer/roblox/main/ItemFarm.lua")()
+end)
+
+
+p:Button("Stand Arrow", function()
+    function StandArrow()
+    game:GetService("ReplicatedStorage").Events.BuyItem:FireServer("MerchantAU","Option3")
+    end
+    StandArrow()
+end)
+
+p:Button("Rokakaka", function()
+    function Rokaka()
+    game:GetService("ReplicatedStorage").Events.BuyItem:FireServer("MerchantAU","Option1")
+    end
+    Rokaka()
+end)
+
+
+w:Button("Bypass", function()
+   Bypass()
+   loadstring(game:HttpGet"https://github.com/PetKunggamer/roblox/blob/main/BypassStandUR.lua")()
+end)
+
+w:Toggle("Auto Farm", function(v)
+    getgenv().AutoFarm = v
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/PetKunggamer/roblox/main/AutoFarm.lua")()
+end)
+
+w:Toggle("Lair Farm", function(v)
+    getgenv().Lair_Farm = v
+   loadstring(game:HttpGet"https://github.com/PetKunggamer/roblox/blob/main/Lair_Farm.lua")()
+end)
+
