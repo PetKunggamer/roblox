2d135cada395cc5a8f1c9d18fef0f745bcf58a42
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2022-07-11 14:47:32 +0700

    Create Check_Gem.lua

---
+++ Check_Gem.lua
@@ -0,0 +1,40 @@
+local placeId = game.PlaceId
+if placeId == 8349889591 then
+    Namek = true
+end
+	
+if Namek then
+repeat wait() until game:IsLoaded()
+wait(10)
+
+local Username = game.Players.LocalPlayer.Character
+local gem = game.Players.LocalPlayer["_stats"]["gem_amount"].Value
+
+local OSTime = os.time();
+local Time = os.date('!*t', OSTime);
+local Avatar = 'https://cdn.discordapp.com/embed/avatars/4.png';
+local Embed = {
+    title = Username.Name;
+    color = '99999';
+    footer = { text = "" };
+    author = {
+        name = 'ROBLOX ACCOUNT';
+        url = 'https://www.roblox.com/';
+    };
+    fields = {
+        {
+            name = 'gem_amount';
+            value = ' :gem:  '..gem ;
+        }
+    };
+    timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
+};
+(syn and syn.request or http_request) {
+    Url = 'https://discord.com/api/webhooks/995948131944517662/aFazcZ0aexZHGJsFZa20LEVLgZWWBuzGG9hU9cbbD26nHRgAe_QmUJdRdwyD2QO7IjEZ';
+    Method = 'POST';
+    Headers = {
+        ['Content-Type'] = 'application/json';
+    };
+    Body = game:GetService'HttpService':JSONEncode( { content = Content; embeds = { Embed } } );
+};
+end
