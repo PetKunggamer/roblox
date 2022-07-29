e45720eb9c571573c0db62981aab473abbfc2aa9
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2022-07-29 22:55:25 +0700

    Update Syn0xz_Hub.lua

--- Syn0xz_Hub.lua
+++ Syn0xz_Hub.lua
@@ -83,53 +83,6 @@ end
 
 create_notification(
     "Syn0xz Hub",
-    "Anti-Kick",
-    nil,
-    function()
-        --// Variables
-
-local Players = game:GetService("Players")
-local OldNameCall = nil
-
---// Global Variables
-
-getgenv().SendNotifications = false -- Set to true if you want to get notified regularly.
-
---// Anti Kick Hook
-
-OldNameCall = hookmetamethod(game, "__namecall", function(Self, ...)
-    local NameCallMethod = getnamecallmethod()
-
-    if tostring(string.lower(NameCallMethod)) == "kick" then
-        if getgenv().SendNotifications == true then
-            game:GetService("StarterGui"):SetCore("SendNotification", {
-                Title = "Exunys Developer",
-                Text = "You almost got kicked! Successfully prevented.",
-                Icon = "rbxassetid://6238540373",
-                Duration = 3,
-            })
-        end
-        
-        return nil
-    end
-    
-    return OldNameCall(Self, ...)
-end)
-
-game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Exunys Developer",
-        Text = "Anti-Kick script loaded",
-        Icon = "rbxassetid://6238537240",
-        Duration = 2,
-    })
-
-    end,
-    "Execute",
-    "No"
-)
-    
-create_notification(
-    "Syn0xz Hub",
     "Anti-Afk",
     nil,
     function()
