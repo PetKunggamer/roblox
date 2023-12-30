c57f742fdcf67fe652d614148516c0e7953ec994
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2023-12-31 06:40:41 +0700

    Update aut.lua

--- aut.lua
+++ aut.lua
@@ -1,5 +1,9 @@
-local Mob = game:GetService("Workspace").Living
-
-for i,v in pairs(Mob:GetChildren()) do
-    print(v)
+function Chest()
+    for i,v in ipairs(Workspace:GetChildren()) do
+        if v.Name == "Common" or v.Name == "Rare" or v.Name == "Epic" or v.Name == "Legendary" and v.Parent then
+            print(v)
+        end
+    end   
 end
+
+Chest()
