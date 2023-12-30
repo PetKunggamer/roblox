3007d22125246eea2d319ed6416d3290a7dac3b9
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2023-12-31 06:47:25 +0700

    Update aut.lua

--- aut.lua
+++ aut.lua
@@ -4,7 +4,7 @@ function Chest()
         local Rare = v:FindFirstChild("Rare")
         local Epic = v:FindFirstChild("Epic")
         local Legendary = v:FindFirstChild("Legendary")
-        if Common or Rare or Epic or Legendary and v.Parent then
+        if Common or Rare or Epic or Legendary then
             print(v)
         end
     end   
