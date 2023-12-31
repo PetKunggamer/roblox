1d5a7d07eab8bdc4df50ee775181e1c44e1ea8ab
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2023-12-31 07:04:45 +0700

    Update aut.lua

--- aut.lua
+++ aut.lua
@@ -1,3 +1,4 @@
+print("Version fireproxmrmpt")
 function Chest()
     for i,v in ipairs(Workspace:GetChildren()) do
         if v.Name == "Common" or v.Name == "Rare" or v.Name == "Epic" or v.Name == "Legendary" and v.Parent then
