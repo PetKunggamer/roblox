6705374e5b776356f6c911fa6f915d9985f0007f
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2024-06-22 13:43:19 +0700

    Update AOT.lua

--- AOT.lua
+++ AOT.lua
@@ -22,9 +22,7 @@ end)
 
 function clickUiButton(v, state)
     local VirtualInputManager = game:GetService('VirtualInputManager')
-    local centerX = v.AbsolutePosition.X + v.AbsoluteSize.X / 2
-    local centerY = v.AbsolutePosition.Y + v.AbsoluteSize.Y / 2
-    VirtualInputManager:SendMouseButtonEvent(centerX, centerY, 0, state, game, 1)
+    VirtualInputManager:SendMouseButtonEvent(v.AbsolutePosition.X + v.AbsoluteSize.X / 2, v.AbsolutePosition.Y + 50, 0, state, game, 1)
 end
 
 local function Rewards()
