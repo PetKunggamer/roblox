08735d3df57a63f5fdf541252bcc896f1ad03ef2
Syn0xz <57756821+PetKunggamer@users.noreply.github.com>
2025-05-12 20:04:15 +0700

    Update FF_edited.lua

--- FF_edited.lua
+++ FF_edited.lua
@@ -96,6 +96,85 @@ local function Attach()
     end
 end
 
+local function Instant()
+    local root = plr.Character and plr.Character:FindFirstChild("HumanoidRootPart")
+    local mob = Get_Mob()
+    if mob then
+        local target = mob:FindFirstChild("HumanoidRootPart")
+        local hum = mob:FindFirstChild("Humanoid")
+        if target and root and hum then
+            hum.Health = 0
+        end
+    end
+end
+
+local function GetMob_List()
+    local mob_table = {}
+    local dist, mob = math.huge, nil
+    local root = plr.Character and plr.Character:FindFirstChild("HumanoidRootPart")
+    for _, v in ipairs(workspace.Alive:GetChildren()) do
+        if v:IsA("Model") and v ~= plr.Character and not game.Players:GetPlayerFromCharacter(v) then
+            local target = v:FindFirstChild("HumanoidRootPart")
+            local hum = v:FindFirstChild('Humanoid')
+            if target and root and hum and hum.Health > 0 then
+                table.insert(mob_table, v)
+            end
+        end
+    end
+    return mob_table
+end
+
+local function Put()
+    local mobList = GetMob_List()
+    for i, v in ipairs(mobList) do
+        local root = v:FindFirstChild("HumanoidRootPart")
+        local hum = v:FindFirstChild("Humanoid")
+        if root and hum then
+            local ESP = root:FindFirstChild("ESP")
+            if not ESP then
+                local billboard = Instance.new("BillboardGui")
+                billboard.Name = "ESP"
+                billboard.Adornee = root
+                billboard.Size = UDim2.new(2, 0, 2, 0)
+                billboard.StudsOffset = Vector3.new(0, 3, 0)
+                billboard.AlwaysOnTop = true
+
+                local Health_Mob = Instance.new("TextLabel")
+                Health_Mob.Size = UDim2.new(1, 0, 1, 0)
+                Health_Mob.BackgroundTransparency = 1
+                Health_Mob.TextColor3 = Color3.fromRGB(255, 0, 0) -- Red text
+                Health_Mob.TextScaled = true
+                Health_Mob.Name = "Health_Mob"
+                Health_Mob.Font = Enum.Font.SourceSansBold
+
+
+                Health_Mob.Parent = billboard
+                billboard.Parent = root
+            end
+        end
+    end
+end
+
+local function UpdateESP()
+    local lp = game.Players.LocalPlayer
+    local char = lp.Character or lp.CharacterAdded:Wait()
+    local root = char:WaitForChild("HumanoidRootPart")
+    for i,v in ipairs(workspace.Alive:GetChildren()) do
+        local hum = v:FindFirstChild("Humanoid")
+        local rootPart = v:FindFirstChild("HumanoidRootPart")
+        if root and hum and rootPart then
+            local ESP = rootPart:FindFirstChild("ESP")
+            local distance = (root.Position - rootPart.Position).Magnitude
+            -- If ESP exists and mob is out of range, remove it
+            if ESP and distance > 100 then
+                ESP:Destroy()
+            elseif ESP then
+                ESP.Health_Mob.Text = math.floor(hum.Health) -- Update health
+            end
+        end
+    end
+end
+
 -- UI
 
 AFK()
@@ -184,8 +263,24 @@ do
         end
     end)
 
-    
+    local Tab_Auto_Instant = Tabs.Main:AddToggle("Tab_Auto_Instant", {Title = "Auto Instant", Default = false })
 
+    Tab_Auto_Instant:OnChanged(function()
+        _G.Auto_Instant = Options.Tab_Auto_Instant.Value
+        while _G.Auto_Instant do task.wait(.01)
+            Instant()
+        end
+    end)
+
+    local Tab_ESP_MOB = Tabs.Main:AddToggle("Tab_ESP_MOB", {Title = "ESP HP MOB", Default = false })
+
+    Tab_ESP_MOB:OnChanged(function()
+        _G.ESP_MOB = Options.Tab_ESP_MOB.Value
+        while _G.ESP_MOB do task.wait(.01)
+            Put()
+            UpdateESP()
+        end
+    end)
 
 
 
